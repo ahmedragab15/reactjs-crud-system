b11635586e7c792b4de71ccb773062df710305e7
ahmedragab15 <ragabahmed154@gmail.com>
2025-03-24 22:37:51 +0200

    install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ function App() {
 
   return (
     <>
-      
+      <h1 className="text-5xl font-bold underline text-red-700 text-center">Hello World</h1>
     </>
-  )
+  );
 }
 
 export default App
